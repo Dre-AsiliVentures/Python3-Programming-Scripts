b56b6fd3285b3757989aa4c8fecf5b70878f1b12
ElectronicsDr <ghdmwendwa001@gmail.com>
2022-05-24 19:59:28 +0300

    Add Lists.py
    Add List.py

---
+++ Lists.py
@@ -0,0 +1,10 @@
+# Creating Lists
+
+#Method 1: Direct Method
+digits=[0,1,'Two']
+
+print ("Method 1: Direct:\n\n      {}\n".format(digits))
+
+#Method 2 : Using lists function
+digits=list([0,1,'Two'])
+print ("Method 2: Indirect using List() function:\n      {}".format(digits))
